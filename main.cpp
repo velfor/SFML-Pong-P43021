1b3bc4086c41a37e90638445fec13bc4c443061c
Paul Nikiforoff <velfor@tut.by>
2022-02-01 17:31:27 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,9 +4,10 @@
 #include "left_bat.h"
 #include "right_bat.h"
 #include "text.h"
-
+enum GameState {INTRO, PLAY, GAME_OVER};
 using namespace sf;
 int main(){
+	GameState game_state = INTRO;
 	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Test");
 	window.setFramerateLimit(FPS);
 	//создание игровых объектов
@@ -16,8 +17,9 @@ int main(){
 		WINDOW_WIDTH - bat_offset - bat_width, 
 		WINDOW_HEIGHT / 2 - bat_half_height
 	);
-	TextObj left_score("",WINDOW_WIDTH/5, 20);
-	TextObj right_score("", 4*WINDOW_WIDTH/5, 20);
+	TextObj left_score("",WINDOW_WIDTH/5, 20,32);
+	TextObj right_score("", 4*WINDOW_WIDTH/5, 20,32);
+	TextObj intro("", WINDOW_WIDTH / 2 - 160, WINDOW_HEIGHT / 2 - 100, 144);
 	while (window.isOpen()) {
 		//обработка событий
 		Event event;
@@ -26,34 +28,53 @@ int main(){
 			if (event.type == Event::Closed)
 				// тогда закрываем его
 				window.close();
+			if (event.type == Event::KeyPressed) {
+				if (event.key.code == Keyboard::Space) {
+					game_state = PLAY;
+				}
+			}
 		}
-		//update игровых объектов
-		ball.update();
-		left_bat.update();
-		right_bat.update();
-		left_score.update(std::to_string(ball.getLeftScore()));
-		right_score.update(std::to_string(ball.getRightScore()));
-		//проверка столкновени€ м€ча и левой ракетки
-		if (ball.getHitBox().intersects(left_bat.getHitBox())) {
-			//ball.setSpeedx(-ball.getSpeedx());
-			ball.reboundX();
+		switch (game_state)
+		{
+		case INTRO:
+			intro.update("PONG");
+			window.clear(Color(50, 50, 50));
+			window.draw(intro.getText());
+			window.display();
+			break;
+		case PLAY:
+			//update игровых объектов
+			ball.update();
+			left_bat.update();
+			right_bat.update();
+			left_score.update(std::to_string(ball.getLeftScore()));
+			right_score.update(std::to_string(ball.getRightScore()));
+			//проверка столкновени€ м€ча и левой ракетки
+			if (ball.getHitBox().intersects(left_bat.getHitBox())) {
+				//ball.setSpeedx(-ball.getSpeedx());
+				ball.reboundX();
+			}
+			//проверка столкновени€ м€ча и правой ракетки
+			if (ball.getHitBox().intersects(right_bat.getHitBox())) {
+				//ball.setSpeedx(-ball.getSpeedx());
+				ball.reboundX();
+			}
+			//отрисовка окна
+			//1 - очищение окна
+			window.clear(Color(50, 50, 50));
+			//2 - отрисовка игровых объектов
+			window.draw(ball.getShape());
+			window.draw(left_bat.getShape());
+			window.draw(right_bat.getShape());
+			window.draw(left_score.getText());
+			window.draw(right_score.getText());
+			//3 - отображение
+			window.display();
+			break;
+		default:
+			break;
 		}
-		//проверка столкновени€ м€ча и правой ракетки
-		if (ball.getHitBox().intersects(right_bat.getHitBox())) {
-			//ball.setSpeedx(-ball.getSpeedx());
-			ball.reboundX();
-		}
-		//отрисовка окна
-		//1 - очищение окна
-		window.clear(Color(50, 50, 50));
-		//2 - отрисовка игровых объектов
-		window.draw(ball.getShape());
-		window.draw(left_bat.getShape());
-		window.draw(right_bat.getShape());
-		window.draw(left_score.getText());
-		window.draw(right_score.getText());
-		//3 - отображение
-		window.display();
+		
 	}
 	return 0;
 }
--- text.h
+++ text.h
@@ -6,11 +6,11 @@ private:
 	sf::Font font;
 	sf::Text text;
 public:
-	TextObj(std::string str, float x, float y) {
+	TextObj(std::string str, float x, float y, size_t size) {
 		font.loadFromFile("ds-digi.ttf");
 		text.setFont(font);
 		text.setString(str);
-		text.setCharacterSize(32);
+		text.setCharacterSize(size);
 		text.setFillColor(sf::Color::Yellow);
 		text.setPosition(x,y);
 	}
